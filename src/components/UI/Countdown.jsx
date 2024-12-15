905643393319656babcf6248e0ae763da07e6842
ahammoud-05 <ayah.hammoud305@gmail.com>
2024-12-15 01:42:42 -0500

    Finished Top Sellers.

--- src/components/UI/Countdown.jsx
+++ src/components/UI/Countdown.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const Countdown = ({ expiryDate }) => {
+  
   const [timeLeft, setTimeLeft] = useState();
 
-  
   useEffect(() => {
 
     const updateCountdown = () => {
