95c892e7c01cef0affb8684630958b14510dbf93
ahammoud-05 <ayah.hammoud305@gmail.com>
2024-12-15 22:42:40 -0500

    Finished Author and Item Details

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+
+  useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+
+  const { authorId } = useParams();
+  const [author, setAuthor] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const res = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`)
+        setTimeout(() => {
+          setAuthor(res.data)
+          setIsLoading(false)
+        }, 1000);
+      }
+      catch (error) {
+        console.log("Error.", error)
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, [authorId]);
+
+  const handleFollowers = () => {
+    if (isFollowing) {
+      setIsFollowing(false)
+      setAuthor((prevFollowers) => ({
+      ...prevFollowers,
+      followers: prevFollowers.followers - 1,
+    }));
+    }
+    else {
+      setIsFollowing(true)
+      setAuthor((prevFollowers) => ({
+        ...prevFollowers,
+        followers: prevFollowers.followers + 1,
+      }));
+    }
+    
+  };
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,28 +74,53 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
+                      { isLoading ? (
+                        <Skeleton width="150px" height="150px" borderRadius="50%" />
+                      ) : (
+                        <img src={author.authorImage} alt="" />
+                      )}
                       <i className="fa fa-check"></i>
+                      
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                          { isLoading ? (
+                            <Skeleton width="150px" height="20px" borderRadius="2px" />
+                          ) : (
+                            <>{author.authorName}</>
+                          )}
+                          { isLoading ? (
+                            null
+                          ) : (
+                            <span className="profile_username">@{author.tag}</span>
+                          )}
+                          { isLoading ? (
+                            null
+                          ) : (
+                            <span id="wallet" className="profile_wallet">
+                            {author.address}
                           </span>
-                          <button id="btn_copy" title="Copy Text">
+                          )}
+                          { isLoading ? (
+                            null 
+                          ) : (
+                            <button id="btn_copy" title="Copy Text">
                             Copy
                           </button>
+                          )}
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
+                      { isLoading ? (
+                        null
+                      ) : (
+                        <div className="profile_follower">{author.followers} followers</div>
+                      )}
+                      
+                      <Link onClick={handleFollowers} to="#" className="btn-main">
+                        { isFollowing ?  "Unfollow" : "Follow"}
                       </Link>
                     </div>
                   </div>
